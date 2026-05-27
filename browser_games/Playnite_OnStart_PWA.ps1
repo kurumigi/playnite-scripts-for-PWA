49dfa8daeae64b8d1be43930658bd4ee07d4ef36
kurumigi <83115+kurumigi@users.noreply.github.com>
2026-05-27 10:18:44 +0900

    Rewrite Playnite_OnStart_PWA.ps1 to ''new method'', which uses a program path and a window title.

--- browser_games/Playnite_OnStart_PWA.ps1
+++ browser_games/Playnite_OnStart_PWA.ps1
@@ -1,35 +1,187 @@
-# ----------------------------------------------------------
-# A tracking script for browser games installed as a PWA.
-# ----------------------------------------------------------
-
-# Path of a browser (Chromium)
+# Path of a browser. (Chromium)
 $BrowserPath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
-# AppID of a PWA game. AppID can be found from the desktop shortcut.
+# AppID of a PWA game. AppID can be found from chrome://apps.
 $GameAppID = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-# Title of a game
+# Title of a game.
 $GameTitle = "GeoGuessr"
 
 # ----------------------------------------------------------
+# Track the playtime of browser games installed as Progressive Web Apps (PWA).
+#
+# Progressive Web Apps (PWA) としてインストールされたブラウザゲームのプレイ時間を追跡します。
+# ----------------------------------------------------------
 
-function Get-WindowsByTitle {
+# Define Win32 APIs to get window information if it hasn't been defined yet.
+# (It's a debugging feature.)
+#
+# まだ定義されていなければ、ウィンドウ情報を取得するために使用する Win32 API を定義します。
+# (これはデバッグ用の機能です。)
+if (-not ([System.Management.Automation.PSTypeName]'GetWindowsWin32').Type)
+{
+    Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+    public static class GetWindowsWin32
+    {
+        // A callback function used with the EnumWindows function.
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        // Enumerates all top-level windows on the screen
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        // Determines the visibility state of the specified window.
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+        // Retrieves the text of the specified window's title bar
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        // Retrieves the length of the specified window's title bar text
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowTextLength(IntPtr hwnd);
+
+        // Retrieves the name of the class to which the specified window belongs.
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        // Retrieves the identifier of the thread that created the specified window and the identifier of the process that created the window.
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        // Sets the show state of a window without waiting for the operation to complete.
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+    }
+"@
+}
+
+# Get a list of main windows.
+#
+# メインウィンドウの一覧を取得します。
+function Get-Windows {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory)]
+        [Parameter()]
+        [Alias("BrowserPath")]
+        [string]$Path,
+        [Parameter()]
+        [Alias("ProcessName")]
+        [string]$Name,
+        [Parameter()]
+        [string]$ClassName,
+        [Parameter()]
+        [Alias("Title")]
         [string]$WindowTitle
     )
 
-    # Window class name of Chromium browsers.
-    $ClassName = "Chrome_WidgetWin_1"
+    # Create a list to store windows informations.
+    $MainWindowList = [System.Collections.Generic.List[object]]::new()
 
-    return [GetWindowsByTitle]::GetWindows($ClassName, $WindowTitle)
+    # Define a callback function.
+    $CallbackFunc = [GetWindowsWin32+EnumWindowsProc]{
+        param(
+            $hWnd,
+            $lParam
+        )
+
+        # Exclude hidden windows.
+        if (-not [GetWindowsWin32]::IsWindowVisible($hWnd))
+        {
+            return $true
+        }
+
+        # Get a length of a window title.
+        $len = [GetWindowsWin32]::GetWindowTextLengthA($hWnd)
+
+        # Exclude windows without window titles.
+        if ($len -le 0)
+        {
+            return $true
+        }
+
+        # Get a window title.
+        $sbTitle = [System.Text.StringBuilder]::new($len + 1)
+        [void][GetWindowsWin32]::GetWindowTextA($hWnd, $sbTitle, $sbTitle.Capacity)
+        $title = $sbTitle.ToString()
+
+        # Get a class name.
+        $sbClass = [System.Text.StringBuilder]::new(256)
+        [void][GetWindowsWin32]::GetClassNameA($hWnd, $sbClass, $sbClass.Capacity)
+        $class = $sbClass.ToString()
+
+        # Get a process id.
+        [uint32]$procId = 0
+        [void][GetWindowsWin32]::GetWindowThreadProcessId($hWnd, [ref]$procId)
+
+        # Get a process information.
+        $proc = Get-Process -Id $procId -ErrorAction SilentlyContinue
+
+        # Exclude windows without process information.
+        if (-not $proc)
+        {
+            return $true
+        }
+
+        # If the $Path argument is provided, exclude where $Name isn't the window process path.
+        if ($Path -and ($Path -ne $proc.Path))
+        {
+            return $true
+        }
+
+        # If the $Name argument is provided, exclude where $Name isn't the window process name.
+        if ($Name -and ($Name -ne $proc.Name))
+        {
+            return $true
+        }
+
+        # If the $ClassName argument is provided, exclude where $ClassName isn't the window class name.
+        if ($ClassName -and ($ClassName -ne $class))
+        {
+            return $true
+        }
+
+        # If the $WindowTitle argument is provided, exclude where $WindowTitle isn't matched in the window title.
+        if ($WindowTitle -and (-not $title.Contains($WindowTitle)))
+        {
+            return $true
+        }
+
+        # Add window information to the list. A little similar to Get-Process.
+        $MainWindowList.Add([pscustomobject]@{
+            Id                  = $procId
+            Name                = $proc.Name
+            ProcessName         = $proc.ProcessName
+            Path                = $proc.Path
+            MainWindowHandle    = $hWnd
+            MainWindowTitle     = $title
+            MainWindowClassName = $class
+        }) | Out-Null
+
+        return $true
+    }
+
+    # Enumerate main windows and get a list of window information.
+    [void][GetWindowsWin32]::EnumWindows($CallbackFunc, [IntPtr]::Zero)
+
+    # Return a list of window information.
+    return $MainWindowList
 }
 
+# Track the playtime of browser games installed as PWA.
+#
+# PWA としてインストールされたブラウザゲームのプレイ時間を追跡します。
 function Start-PWAProcess {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory,Position=0)]
         [Alias("BrowserPath")]
-        [string]$FilePath,
+        [string]$Path,
         [Parameter(Mandatory,Position=1)]
         [Alias("AppID")]
         [string]$ArgumentList,
@@ -38,33 +190,42 @@ function Start-PWAProcess {
         [string]$WindowTitle
     )
 
+    # Define the flag.
     $BrowserRunning = $false
 
-    Start-Process -FilePath $FilePath -ArgumentList "--app-id=`"${ArgumentList}`""
+    # Open a PWA.
+    Start-Process -FilePath $Path -ArgumentList "--app-id=`"${ArgumentList}`""
 
+    # Loop for waiting.
     while ($true)
     {
-        # Check if game window is opening
-        $Browser = Get-WindowsByTitle($WindowTitle)
+        # Get the window information to check if a game window is open.
+        $Browser = Get-Windows -Path $Path -WindowTitle $WindowTitle
 
-        # if game window opened
+        # if a game window is open.
         if (!$BrowserRunning -and ($Browser.Length -ne 0))
         {
+            # Set the flag.
             $BrowserRunning = $true
-            [GetWindowsWin32]::ShowWindowAsync($Browser.Item2, 3) # SW_MAXIMIZE
+
+            # Maximize a game window. (3 is SW_MAXIMIZE)
+            [void][GetWindowsWin32]::ShowWindowAsync($Browser.MainWindowHandle, 3)
         }
 
-        # if game window closed
+        # if a game window is closed.
         if ($BrowserRunning -and ($Browser.Length -eq 0))
         {
+            # Unset the flag.
             $BrowserRunning = $false
+
+            # Break the loop.
             break
         }
 
-        # Sleep for a while to not waste CPU
+        # Sleep for a while to not waste CPU.
         Start-Sleep -s 1
     }
 }
 
-# Start a game
+# Start a game.
 Start-PWAProcess -BrowserPath $BrowserPath -AppID $GameAppID -Title $GameTitle
