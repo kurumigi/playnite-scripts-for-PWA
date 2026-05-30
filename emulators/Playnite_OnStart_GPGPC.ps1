7679921ac8487eb3827d3908d3a4ceadaa15ffad
kurumigi <83115+kurumigi@users.noreply.github.com>
2026-05-30 15:03:32 +0900

    Removes the ''Debugging Features''.
    For debugging, it is recommended to use tools like Playnite's ''Interactive PowerShell Console''.
    (See https://api.playnite.link/docs/tutorials/extensions/scriptingDebugging.html)

--- emulators/Playnite_OnStart_GPGPC.ps1
+++ emulators/Playnite_OnStart_GPGPC.ps1
@@ -19,56 +19,6 @@ $GameTitle = "スイカゲーム-Aladdin X"
 #
 # ----------------------------------------------------------
 
-# Define Win32 APIs to get window information if it hasn't been defined yet.
-# (It's a debugging feature.)
-#
-# まだ定義されていなければ、ウィンドウ情報を取得するために使用する Win32 API を定義します。
-# (これはデバッグ用の機能です。)
-if (-not ([System.Management.Automation.PSTypeName]'GetWindowsWin32').Type)
-{
-    Add-Type @"
-using System;
-using System.Runtime.InteropServices;
-using System.Text;
-    public static class GetWindowsWin32
-    {
-        // A callback function used with the EnumWindows function.
-        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
-
-        // Enumerates all top-level windows on the screen
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
-
-        // Determines the visibility state of the specified window.
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool IsWindowVisible(IntPtr hWnd);
-
-        // Retrieves the text of the specified window's title bar
-        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
-
-        // Retrieves the length of the specified window's title bar text
-        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        public static extern int GetWindowTextLength(IntPtr hwnd);
-
-        // Retrieves the name of the class to which the specified window belongs.
-        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
-
-        // Retrieves the identifier of the thread that created the specified window and the identifier of the process that created the window.
-        [DllImport("user32.dll")]
-        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
-
-        // Sets the show state of a window without waiting for the operation to complete.
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
-    }
-"@
-}
-
 # Get a list of main windows.
 #
 # メインウィンドウの一覧を取得します。
