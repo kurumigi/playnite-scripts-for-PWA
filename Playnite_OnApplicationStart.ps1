1850f3e3e2bc2feb703dd0894934a704439af249
kurumigi <83115+kurumigi@users.noreply.github.com>
2026-06-03 02:15:52 +0900

    Revert "Remove codes for ''old method''.", because reusing this code to port ''new method'' to C#.
    This reverts commit f496a911cd552a38b9283d9acda17130d156a22b.

--- Playnite_OnApplicationStart.ps1
+++ Playnite_OnApplicationStart.ps1
@@ -22,6 +22,13 @@ using System;
 using System.Runtime.InteropServices;
 using System.Text;
 
+// Old method code
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Security;
+// Old method code
+
     public static class GetWindowsWin32
     {
         // A callback function used with the EnumWindows function.
@@ -58,4 +65,69 @@ using System.Text;
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
     }
+
+// Old method code
+    // A class to get a list of windows with a specific class name.
+    //
+    // あるクラス名を持つウィンドウの一覧を取得するクラス
+    public class GetWindowsByClassName
+    {
+
+        private GetWindowsByClassName(string className)
+        {
+            _className = className;
+            GetWindowsWin32.EnumWindows(callback, IntPtr.Zero);
+        }
+
+        private bool callback(IntPtr hWnd, IntPtr lparam)
+        {
+            if (GetWindowsWin32.GetClassName(hWnd, _apiResult, _apiResult.Capacity) != 0)
+            {
+                if (string.CompareOrdinal(_apiResult.ToString(), _className) == 0)
+                {
+                    _result.Add(hWnd);
+                }
+            }
+
+            return true;
+        }
+
+        public static IEnumerable<Tuple<string, IntPtr>> GetWindows(string className)
+        {
+            foreach (var windowHandle in GetWindowHandles(className))
+            {
+                int length = GetWindowsWin32.GetWindowTextLength(windowHandle);
+                StringBuilder sb = new StringBuilder(length + 1);
+                GetWindowsWin32.GetWindowText(windowHandle, sb, sb.Capacity);
+                yield return new Tuple<string, IntPtr>(sb.ToString(), windowHandle);
+            }
+        }
+
+        public static List<IntPtr> GetWindowHandles(string className)
+        {
+            if (string.IsNullOrWhiteSpace(className))
+                throw new ArgumentOutOfRangeException("className", className, "className can't be null or blank.");
+
+            return new GetWindowsByClassName(className)._result;
+        }
+
+        private readonly string _className;
+        private readonly List<IntPtr> _result = new List<IntPtr>();
+        private readonly StringBuilder _apiResult = new StringBuilder(1024);
+    }
+
+    // A class to get a list of windows with a specific class name and window title.
+    //
+    // あるクラス名とタイトルを持つウィンドウの一覧を取得するクラス
+    public class GetWindowsByTitle
+    {
+        public static IEnumerable<Tuple<string, IntPtr>> GetWindows(string className, string titlePrefix)
+        {
+            // Check window title
+            foreach (var tuple in GetWindowsByClassName.GetWindows(className))
+                if (tuple.Item1.Contains(titlePrefix))
+                    yield return new Tuple<string, IntPtr>(tuple.Item1, tuple.Item2);
+        }
+    }
+// Old method code
 "@
