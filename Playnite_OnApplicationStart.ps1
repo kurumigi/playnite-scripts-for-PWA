7d0b12fd4b063c81d727eb17d1044c12316dfbb1
kurumigi <83115+kurumigi@users.noreply.github.com>
2026-06-05 01:33:26 +0900

    Port the Get-Windows cmdlet to C# and relocate the cmdlet from scripts executed as play actions to those executed during Playnite startup.
    As a result, reduce duplicate code in play action scripts and improve maintainability.
    And, remove Reference_Get-Windows.ps1 as it is no longer needed following the porting process.

--- Playnite_OnApplicationStart.ps1
+++ Playnite_OnApplicationStart.ps1
@@ -5,30 +5,440 @@
 # ----------------------------------------------------------
 #
 # Reference / Source:
-#   Old method (ClassName and WindowTitle):
+#   Old method (ClassName and WindowTitle / C#):
 #     https://stackoverflow.com/questions/16958051/get-chrome-browser-title-using-c-sharp
 #     https://raykeymas.com/posts/powershell/get-chrome-window (Japanese)
-#   Current method (Program Name and WindowTitle):
+#   Current method (Program Name and WindowTitle / Powershell):
 #     https://qiita.com/Tadataka_Takahashi/items/91c42661ef9559ac5f86 (Japanese)
 #     https://note.com/kaito_mishima/n/n99cdce0b72f0 (Japanese)
+#   Current method (Program Name and WindowTitle / C#):
+#     https://learn.microsoft.com/en-us/powershell/scripting/developer/cmdlet/cmdlet-overview?view=powershell-5.1
+#   Imports a cmdlet written in C# within a .ps1 file as a PowerShell module:
+#     https://devblogs.microsoft.com/powershell/dynamic-binary-modules/
+#     https://gist.github.com/guitarrapc/e398672bcb26814b6328
 #
 # ----------------------------------------------------------
 
-# Define Win32 APIs to get window information
+# Get a list of main windows.
 #
-# ウィンドウ情報を取得するために使用する Win32 API を定義します。
-Add-Type @"
+# メインウィンドウの一覧を取得します。
+$CSData = Add-Type @"
 using System;
-using System.Runtime.InteropServices;
-using System.Text;
-
-// Old method code
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Security;
-// Old method code
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Management.Automation;    // Windows PowerShell namespace.
+using System.Text;
 
+    #region GetWindowsCommand
+    /// <summary>
+    /// This class implements the Get-Windows cmdlet.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "Windows")]
+    public class GetWindowsCommand : Cmdlet
+    {
+        #region private Members
+        /// <summary>
+        /// This list stores handles of current windows.
+        /// </summary>
+        private List<IntPtr> windowHandles;
+
+        /// <summary>
+        /// This array stores information about current processes.
+        /// </summary>
+        private Process[] processes;
+        #endregion private Members
+
+        #region Parameters
+        /// <summary>
+        /// The process IDs of the windows to act on.
+        /// </summary>
+        private uint[] Ids;
+
+        /// <summary>
+        /// Gets or sets the list of process IDs on
+        /// which the Get-Windows cmdlet will work.
+        /// </summary>
+        [Parameter(
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public uint[] Id
+        {
+            get { return this.Ids; }
+            set { this.Ids = value; }
+        }
+
+        /// <summary>
+        /// The process paths of the windows to act on.
+        /// </summary>
+        private string[] processPaths;
+
+        /// <summary>
+        /// Gets or sets the list of process paths on
+        /// which the Get-Windows cmdlet will work.
+        /// </summary>
+        [Parameter(
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        [Alias("Path")]
+        public string[] ProcessPath
+        {
+            get { return this.processPaths; }
+            set { this.processPaths = value; }
+        }
+
+        /// <summary>
+        /// The process names of the windows to act on.
+        /// </summary>
+        private string[] processNames;
+
+        /// <summary>
+        /// Gets or sets the list of process names on
+        /// which the Get-Windows cmdlet will work.
+        /// </summary>
+        [Parameter(
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        [Alias("Name")]
+        public string[] ProcessName
+        {
+            get { return this.processNames; }
+            set { this.processNames = value; }
+        }
+
+        /// <summary>
+        /// The ClassNames of the windows to act on.
+        /// </summary>
+        private string[] classNames;
+
+        /// <summary>
+        /// Gets or sets the list of ClassNames on
+        /// which the Get-Windows cmdlet will work.
+        /// </summary>
+        [Parameter(
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string[] ClassName
+        {
+            get { return this.classNames; }
+            set { this.classNames = value; }
+        }
+
+        /// <summary>
+        /// The titles of the windows to act on.
+        /// </summary>
+        private string[] windowTitles;
+
+        /// <summary>
+        /// Gets or sets the list of titles on
+        /// which the Get-Windows cmdlet will work.
+        /// </summary>
+        [Parameter(
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        [Alias("Title")]
+        public string[] WindowTitle
+        {
+            get { return this.windowTitles; }
+            set { this.windowTitles = value; }
+        }
+        #endregion Parameters
+
+        #region Cmdlet Overrides
+        /// <summary>
+        /// The BeginProcessing method retrieves
+        /// the current windows and the current processes.
+        /// </summary>
+        protected override void BeginProcessing()
+        {
+            // Retrieve the current windows.
+            windowHandles = GetWindowsList.GetWindows();
+
+            // Retrieve the current processes.
+            processes = Process.GetProcesses();
+        }
+
+        /// <summary>
+        /// The ProcessRecord method calls the Process.GetProcesses
+        /// method to retrieve the processes specified by the Name
+        /// parameter. Then, the WriteObject method writes the
+        /// associated processes to the pipeline.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            foreach (var windowHandle in windowHandles)
+            {
+                // Exclude hidden windows.
+                if (!GetWindowsWin32.IsWindowVisible(windowHandle))
+                {
+                    continue;
+                }
+
+                // Get a length of a window title.
+                int windowTltleLength = GetWindowsWin32.GetWindowTextLength(windowHandle);
+
+                // Exclude windows without window titles.
+                if (windowTltleLength == 0)
+                {
+                    continue;
+                }
+
+                // Get a window title.
+                StringBuilder windowTitleBuilder = new StringBuilder(windowTltleLength + 1);
+                GetWindowsWin32.GetWindowText(windowHandle, windowTitleBuilder, windowTitleBuilder.Capacity);
+                String windowTitle = windowTitleBuilder.ToString();
+
+                // Get a class name.
+                StringBuilder windowClassNameBuilder = new StringBuilder(256);
+                GetWindowsWin32.GetClassName(windowHandle, windowClassNameBuilder, windowClassNameBuilder.Capacity);
+                String windowClassName = windowClassNameBuilder.ToString();
+
+                // Get a process id.
+                uint processId = 0;
+                GetWindowsWin32.GetWindowThreadProcessId(windowHandle, out processId);
+
+                // Get a process information.
+                Process windowProcess = processes.FirstOrDefault(p => p.Id == processId);
+
+                // Exclude windows without process information.
+                if (windowProcess == null)
+                {
+                    continue;
+                }
+
+
+                // Try to retrive Process path from Process.MainModule.FileName.
+                String windowProcessPath = "";
+
+                try {
+                    windowProcessPath = windowProcess.MainModule.FileName;
+                }
+                catch (Win32Exception e)
+                {
+                    // Ignore failed to access error.
+                    if (e.ErrorCode == unchecked((int) 0x80004005)) // E_FAIL
+                    {
+                        WriteVerbose("Failed to access process \"" + windowProcess.ProcessName + "\".");
+                    }
+                    // Throw other errors.
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                // If the Id argument is provided,
+                // exclude where Id isn't match the window process path.
+                if (!checkArgument(Ids, processId))
+                {
+                    continue;
+                }
+
+                // If the ProcessPath argument is provided,
+                // exclude where ProcessPath isn't match the window process path.
+                if (!checkArgument(processPaths, windowProcessPath))
+                {
+                    continue;
+                }
+
+                // If the ProcessName argument is provided,
+                // exclude where ProcessName isn't match the window process name.
+                if (!checkArgument(processNames, windowProcess.ProcessName))
+                {
+                    continue;
+                }
+
+                // If the ClassName argument is provided,
+                // exclude where ClassName isn't match the window className.
+                if (!checkArgument(classNames, windowClassName))
+                {
+                    continue;
+                }
+
+                // If the WindowTitle argument is provided,
+                // exclude where $WindowTitle isn't matched in the window title.
+                if (!checkArgumentContains(windowTitles, windowTitle))
+                {
+                    continue;
+                }
+
+                // Add window information to the list. A little similar to Get-Process.
+                GetWindowsResult result = new GetWindowsResult();
+                result._Id                  = processId;
+                result._ProcessName         = windowProcess.ProcessName;
+                result._Path                = windowProcessPath;
+                result._MainWindowHandle    = windowHandle.ToInt32();
+                result._MainWindowTitle     = windowTitle;
+                result._MainWindowClassName = windowClassName;
+
+                // Write window information to the pipeline
+                // to make them available to the next cmdlet.
+                WriteObject(result);
+            }
+        }
+        #endregion Cmdlet Overrides
+
+        #region private functions
+        /// <summary>
+        /// This function compares a numeric array with a numeric value
+        /// to determine whether there is a match.
+        /// If the numeric array is empty,
+        /// all numeric values are treated as "matching".
+        /// </summary>
+        private bool checkArgument(uint[] arguments, uint checkUint)
+        {
+            bool checkResult = false;
+
+            if (arguments == null)
+            {
+                checkResult = true;
+                return checkResult;
+            }
+
+            foreach (uint argument in arguments)
+            {
+                if (argument == checkUint)
+                {
+                    checkResult = true;
+                    break;
+                }
+            }
+
+            return checkResult;
+        }
+
+        /// <summary>
+        /// This function compares a string array with a string
+        /// to determine whether there is a match.
+        /// If the string array is empty,
+        /// all strings are treated as "matching".
+        /// </summary>
+        private bool checkArgument(string[] arguments, string checkString)
+        {
+            bool checkResult = false;
+
+            if (arguments == null)
+            {
+                checkResult = true;
+                return checkResult;
+            }
+
+            foreach (string argument in arguments)
+            {
+                if (argument == checkString)
+                {
+                    checkResult = true;
+                    break;
+                }
+            }
+
+            return checkResult;
+        }
+
+        /// <summary>
+        /// This function compares a string array with a string
+        /// to determine whether any strings partially match.
+        /// If the string array is empty,
+        /// all strings are treated as "matching".
+        /// </summary>
+        private bool checkArgumentContains(string[] arguments, string checkString)
+        {
+            bool checkResult = false;
+
+            if (arguments == null)
+            {
+                checkResult = true;
+                return checkResult;
+            }
+
+            foreach (string argument in arguments)
+            {
+                if (checkString.Contains(argument))
+                {
+                    checkResult = true;
+                    break;
+                }
+            }
+
+            return checkResult;
+        }
+        #endregion private functions
+    }
+    #endregion GetWindowsCommand
+
+    #region GetWindowsResult
+    /// <summary>
+    /// This class implements the return value of the Get-Windows cmdlet.
+    /// </summary>
+    public class GetWindowsResult
+    {
+        #region Members
+        internal uint   _Id;                  // Process.Id
+        internal string _ProcessName;         // Process.ProcessName
+        internal string _Path;                // Process.MainModule.FileName
+        internal int    _MainWindowHandle;    // Process.MainWindowHandle
+        internal string _MainWindowTitle;     // Process.MainWindowTitle
+        internal string _MainWindowClassName;
+        #endregion Members
+
+        #region Properties
+        public uint Id
+        {
+            get { return this._Id; }
+        }
+
+        public string Name
+        {
+            get { return this._ProcessName; }
+        }
+
+        public string ProcessName
+        {
+            get { return this._ProcessName; }
+        }
+
+        public string Path
+        {
+            get { return this._Path; }
+        }
+
+        public int MainWindowHandle
+        {
+            get { return this._MainWindowHandle; }
+        }
+
+        public string MainWindowTitle
+        {
+            get { return this._MainWindowTitle; }
+        }
+
+        public string MainWindowClassName
+        {
+            get { return this._MainWindowClassName; }
+        }
+        #endregion Properties
+
+        #region Methods
+        public override string ToString()
+        {
+            return "{Id=" + Id.ToString()
+                + ", ProcessName=" + ProcessName
+                + ", Path=" + Path
+                + ", MainWindowHandle=" + MainWindowHandle.ToString()
+                + ", MainWindowTitle=" + MainWindowTitle
+                + ", MainWindowClassName=" + MainWindowClassName + "}";
+        }
+        #endregion Methods
+    }
+    #endregion Window
+
+    #region GetWindowsWin32
+    /// <summary>
+    /// This class provides access to Win32 APIs.
+    /// </summary>
     public static class GetWindowsWin32
     {
         // A callback function used with the EnumWindows function.
@@ -65,69 +475,38 @@ using System.Security;
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
     }
+    #endregion GetWindowsWin32
 
-// Old method code
-    // A class to get a list of windows with a specific class name.
-    //
-    // あるクラス名を持つウィンドウの一覧を取得するクラス
-    public class GetWindowsByClassName
+    #region GetWindowsList
+    /// <summary>
+    /// This class retrieves a list of main windows.
+    /// </summary>
+    internal class GetWindowsList
     {
 
-        private GetWindowsByClassName(string className)
+        private GetWindowsList()
         {
-            _className = className;
             GetWindowsWin32.EnumWindows(callback, IntPtr.Zero);
         }
 
         private bool callback(IntPtr hWnd, IntPtr lparam)
         {
-            if (GetWindowsWin32.GetClassName(hWnd, _apiResult, _apiResult.Capacity) != 0)
-            {
-                if (string.CompareOrdinal(_apiResult.ToString(), _className) == 0)
-                {
-                    _result.Add(hWnd);
-                }
-            }
-
+            _result.Add(hWnd);
             return true;
         }
 
-        public static IEnumerable<Tuple<string, IntPtr>> GetWindows(string className)
+        public static List<IntPtr> GetWindows()
         {
-            foreach (var windowHandle in GetWindowHandles(className))
-            {
-                int length = GetWindowsWin32.GetWindowTextLength(windowHandle);
-                StringBuilder sb = new StringBuilder(length + 1);
-                GetWindowsWin32.GetWindowText(windowHandle, sb, sb.Capacity);
-                yield return new Tuple<string, IntPtr>(sb.ToString(), windowHandle);
-            }
+            return new GetWindowsList()._result;
         }
 
-        public static List<IntPtr> GetWindowHandles(string className)
-        {
-            if (string.IsNullOrWhiteSpace(className))
-                throw new ArgumentOutOfRangeException("className", className, "className can't be null or blank.");
-
-            return new GetWindowsByClassName(className)._result;
-        }
-
-        private readonly string _className;
         private readonly List<IntPtr> _result = new List<IntPtr>();
-        private readonly StringBuilder _apiResult = new StringBuilder(1024);
     }
+    #endregion GetWindowsList
+"@ -PassThru
 
-    // A class to get a list of windows with a specific class name and window title.
-    //
-    // あるクラス名とタイトルを持つウィンドウの一覧を取得するクラス
-    public class GetWindowsByTitle
-    {
-        public static IEnumerable<Tuple<string, IntPtr>> GetWindows(string className, string titlePrefix)
-        {
-            // Check window title
-            foreach (var tuple in GetWindowsByClassName.GetWindows(className))
-                if (tuple.Item1.Contains(titlePrefix))
-                    yield return new Tuple<string, IntPtr>(tuple.Item1, tuple.Item2);
-        }
-    }
-// Old method code
-"@
+# Imports a cmdlet written in C# within a .ps1 file as a PowerShell module.
+#
+# .ps1ファイル内にC#で記述されたコマンドレットを、
+# Powershellのモジュールとしてインポートします。
+Import-Module -Assembly $CSData.Assembly -PassThru
--- browser_games/Playnite_OnStart_Webapp.ps1
+++ browser_games/Playnite_OnStart_Webapp.ps1
@@ -12,126 +12,19 @@ $GameTitle = "slither.io"
 # ----------------------------------------------------------
 #
 # Reference / Source:
-#   Old method (ClassName and WindowTitle):
+#   Old method (ClassName and WindowTitle / C#):
 #     https://stackoverflow.com/questions/16958051/get-chrome-browser-title-using-c-sharp
 #     https://raykeymas.com/posts/powershell/get-chrome-window (Japanese)
-#   Current method (Program Name and WindowTitle):
+#   Current method (Program Name and WindowTitle / Powershell):
 #     https://qiita.com/Tadataka_Takahashi/items/91c42661ef9559ac5f86 (Japanese)
 #     https://note.com/kaito_mishima/n/n99cdce0b72f0 (Japanese)
+#   Current method (Program Name and WindowTitle / C#):
+#     https://learn.microsoft.com/en-us/powershell/scripting/developer/cmdlet/cmdlet-overview?view=powershell-5.1
+#   Imports a cmdlet written in C# within a .ps1 file as a PowerShell module:
+#     https://devblogs.microsoft.com/powershell/dynamic-binary-modules/
+#     https://gist.github.com/guitarrapc/e398672bcb26814b6328
 #
 # ----------------------------------------------------------
-
-# Get a list of main windows.
-#
-# メインウィンドウの一覧を取得します。
-function Get-Windows {
-    [CmdletBinding()]
-    param (
-        [Parameter()]
-        [Alias("ProcessPath")]
-        [string]$Path,
-        [Parameter()]
-        [Alias("ProcessName")]
-        [string]$Name,
-        [Parameter()]
-        [string]$ClassName,
-        [Parameter()]
-        [Alias("Title")]
-        [string]$WindowTitle
-    )
-
-    # Create a list to store windows informations.
-    $MainWindowList = [System.Collections.Generic.List[object]]::new()
-
-    # Define a callback function.
-    $CallbackFunc = [GetWindowsWin32+EnumWindowsProc]{
-        param(
-            $hWnd,
-            $lParam
-        )
-
-        # Exclude hidden windows.
-        if (-not [GetWindowsWin32]::IsWindowVisible($hWnd))
-        {
-            return $true
-        }
-
-        # Get a length of a window title.
-        $len = [GetWindowsWin32]::GetWindowTextLength($hWnd)
-
-        # Exclude windows without window titles.
-        if ($len -le 0)
-        {
-            return $true
-        }
-
-        # Get a window title.
-        $sbTitle = [System.Text.StringBuilder]::new($len + 1)
-        [void][GetWindowsWin32]::GetWindowText($hWnd, $sbTitle, $sbTitle.Capacity)
-        $title = $sbTitle.ToString()
-
-        # Get a class name.
-        $sbClass = [System.Text.StringBuilder]::new(256)
-        [void][GetWindowsWin32]::GetClassName($hWnd, $sbClass, $sbClass.Capacity)
-        $class = $sbClass.ToString()
-
-        # Get a process id.
-        [uint32]$procId = 0
-        [void][GetWindowsWin32]::GetWindowThreadProcessId($hWnd, [ref]$procId)
-
-        # Get a process information.
-        $proc = Get-Process -Id $procId -ErrorAction SilentlyContinue
-
-        # Exclude windows without process information.
-        if (-not $proc)
-        {
-            return $true
-        }
-
-        # If the $Path argument is provided, exclude where $Name isn't the window process path.
-        if ($Path -and ($Path -ne $proc.Path))
-        {
-            return $true
-        }
-
-        # If the $Name argument is provided, exclude where $Name isn't the window process name.
-        if ($Name -and ($Name -ne $proc.Name))
-        {
-            return $true
-        }
-
-        # If the $ClassName argument is provided, exclude where $ClassName isn't the window class name.
-        if ($ClassName -and ($ClassName -ne $class))
-        {
-            return $true
-        }
-
-        # If the $WindowTitle argument is provided, exclude where $WindowTitle isn't matched in the window title.
-        if ($WindowTitle -and (-not $title.Contains($WindowTitle)))
-        {
-            return $true
-        }
-
-        # Add window information to the list. A little similar to Get-Process.
-        $MainWindowList.Add([pscustomobject]@{
-            Id                  = $procId
-            Name                = $proc.Name
-            ProcessName         = $proc.ProcessName
-            Path                = $proc.Path
-            MainWindowHandle    = $hWnd
-            MainWindowTitle     = $title
-            MainWindowClassName = $class
-        }) | Out-Null
-
-        return $true
-    }
-
-    # Enumerate main windows and get a list of window information.
-    [void][GetWindowsWin32]::EnumWindows($CallbackFunc, [IntPtr]::Zero)
-
-    # Return a list of window information.
-    return $MainWindowList
-}
 
 # Track the playtime of (non-PWA) browser games.
 #
@@ -164,6 +57,10 @@ function Start-WebProcess {
     {
         # Get the window information to check if a game window is open.
         $Browser = Get-Windows -Name $Name -WindowTitle $WindowTitle
+
+        # Write the window information to the information stream.
+        $BrowserInfo = "${Browser} = Get-Windows -Name ${Name} -WindowTitle ${WindowTitle}"
+        Write-Information $BrowserInfo
 
         # if a game window is open.
         if (!$BrowserRunning -and ($Browser.Length -ne 0))
